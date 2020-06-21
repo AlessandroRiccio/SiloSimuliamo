66e0b58b0050d60e729d0250de59ebfaf8d1b5bd
Alessandro Riccio <alessandro.riccio@stud.tecnicosuperiorekennedy.it>
2020-06-22 01:14:02 +0200

    Initial Commit

---
+++ Models/Silo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Silo
+    {
+        public int IdSilos { get; set; }
+
+        public int Capacita { get; set; }
+
+        public int Riempimento { get; set; }
+    }
+}
